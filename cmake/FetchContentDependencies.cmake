04d9f796fea9a3f1a778393e945e4d10bb0292ff
Alex Beimler <alex-beimler@web.de>
2020-02-26 20:39:06 +0100

    clean up

--- cmake/FetchContentDependencies.cmake
+++ cmake/FetchContentDependencies.cmake
@@ -6,37 +6,37 @@ include(FetchContent)
 # remove libs you don't use
 ##
 
-## Embedded Template Library, just for fun not needed
-FetchContent_Declare(
-    etl
-    GIT_REPOSITORY https://github.com/ETLCPP/etl.git
-    GIT_TAG        14.35.1
-)
-if(NOT etl_POPULATED)
-  FetchContent_Populate(etl)
-  add_subdirectory(${etl_SOURCE_DIR} ${etl_BINARY_DIR})
-endif()
+# ## Embedded Template Library, just for fun not needed
+# FetchContent_Declare(
+#     etl
+#     GIT_REPOSITORY https://github.com/ETLCPP/etl.git
+#     GIT_TAG        14.35.1
+# )
+# if(NOT etl_POPULATED)
+#   FetchContent_Populate(etl)
+#   add_subdirectory(${etl_SOURCE_DIR} ${etl_BINARY_DIR})
+# endif()
 
-## nice to have, not needed
-FetchContent_Declare(
-    units
-    GIT_REPOSITORY https://github.com/nholthaus/units.git
-    GIT_TAG        v2.3.1
-)
-if(NOT units_POPULATED)
-  FetchContent_Populate(units)
+# ## nice to have, not needed
+# FetchContent_Declare(
+#     units
+#     GIT_REPOSITORY https://github.com/nholthaus/units.git
+#     GIT_TAG        v2.3.1
+# )
+# if(NOT units_POPULATED)
+#   FetchContent_Populate(units)
 
-  #if (NOT BUILD_TESTS) # optimizaions, units needed for tests
-  #  set(DISABLE_IOSTREAM                  ON CACHE BOOL "" FORCE)
-  #  set(DISABLE_PREDEFINED_UNITS          ON CACHE BOOL "" FORCE)
+#   #if (NOT BUILD_TESTS) # optimizaions, units needed for tests
+#   #  set(DISABLE_IOSTREAM                  ON CACHE BOOL "" FORCE)
+#   #  set(DISABLE_PREDEFINED_UNITS          ON CACHE BOOL "" FORCE)
 
-  #  set(ENABLE_PREDEFINED_LENGTH_UNITS    ON CACHE BOOL "" FORCE)
-  #  set(ENABLE_PREDEFINED_MASS_UNITS      ON CACHE BOOL "" FORCE)
-  #  set(ENABLE_PREDEFINED_TIME_UNITS      ON CACHE BOOL "" FORCE)
-  #endif()
+#   #  set(ENABLE_PREDEFINED_LENGTH_UNITS    ON CACHE BOOL "" FORCE)
+#   #  set(ENABLE_PREDEFINED_MASS_UNITS      ON CACHE BOOL "" FORCE)
+#   #  set(ENABLE_PREDEFINED_TIME_UNITS      ON CACHE BOOL "" FORCE)
+#   #endif()
 
-  add_subdirectory(${units_SOURCE_DIR} ${units_BINARY_DIR})
-endif()
+#   add_subdirectory(${units_SOURCE_DIR} ${units_BINARY_DIR})
+# endif()
 
 ## need for benchmarks
 FetchContent_Declare(
